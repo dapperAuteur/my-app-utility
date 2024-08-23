89cdfc14f2a9737054e45f8b72dee2d7d0f158ab
awe <aweful@awews.com>
2024-08-22 23:35:30 -0400

    add page to show account by id

---
+++ app/accounts/[_id]/page.jsx
@@ -0,0 +1,47 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+
+function Account ({params}) {
+  // console.log('5 client params :>> ', params);
+  const { _id } = params;
+
+  const [account, setAccount] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchAccount() {
+      try {
+        const res = await fetch(`/api/accounts/${_id}`);
+        if (!res.ok) {
+          throw new Error("Failed to fetch Account");
+        }
+        const result = await res.json();
+        // console.log('result :>> ', result);
+        setAccount(result.account[0]);
+      } catch (error) {
+        console.log('error :>> ', error);
+        setError(error.message);
+      }
+    }
+    fetchAccount();
+  
+  }, [_id]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!account) {
+    return <div>Loading...</div>;
+  }  
+
+  return (
+    <div>
+      <h2>Account</h2>
+      <h3>{account.accountName}</h3>
+      <h3>{account.accountType}</h3>
+    </div>
+  )
+}
+
+export default Account
