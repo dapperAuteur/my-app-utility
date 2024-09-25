11c0054bb2f74c0e021911a24450c57929c9533d
awe <aweful@awews.com>
2024-09-24 22:01:00 -0400

    refactor: add handleDelete, error message, protect edit/delete
    refactored to restrict edit and delete to logged in users

--- app/tags/[_id]/page.jsx
+++ app/tags/[_id]/page.jsx
@@ -2,14 +2,40 @@
 // this gives access to params
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 
 function Tag({params}) {
   // console.log('client params :>> ', params);
+  const router = useRouter();
   const { _id } = params;
+  const { data: session } = useSession();
 
   const [tag, setTag] = useState(null);
-  const [error, setError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleDelete = async (session, tagId) => {
+    console.log('tagId :>> ', tagId);
+    if (!session) {
+      setErrorMessage("Tag NOT DELETED! Must be Admin to delete Tag.");
+      return;
+    }
+    try {
+      const res = await fetch(`/api/tags/${tagId}`, {
+        method: "DELETE",
+        body: JSON.stringify(session),
+        "content-type": "application/json",
+      });
+      setSuccessMessage(res.statusText);
+      router.refresh();
+      router.push("/tags/");
+    } catch (error) {
+      console.log('error :>> ', error);
+      setErrorMessage(error);
+    }
+  }
 
   useEffect(() => {
     async function fetchTag() {
@@ -31,27 +57,30 @@ function Tag({params}) {
   
   // console.log('client tag :>> ', tag);
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
   if (!tag) {
     return <div>Loading...</div>;
   }  
 
   return (
     <div className='m-2'>
-      <h2>Tag</h2>
+      <Link href={"/tags"}><h2>Tag</h2></Link>
+      <p className='text-red-500'>{errorMessage}</p>
+      <p className='text-green-500'>{successMessage}</p>
       <h3>{tag.tag_name}</h3>
       <h3>{tag.description}</h3>
-      <div>
-        <Link href={`/tags/${tag._id}/edit`}>Edit</Link>
-      </div>
-      <div>
-        <button className='border-double border-2'
-          onClick={() => handleDelete(tag._id)}
-          >Delete</button>
-      </div>
+      { session ? 
+        <div>
+          <div>
+            <Link href={`/tags/${tag._id}/edit`}>
+              Edit
+            </Link>
+          </div>
+          <div className='border-double border-2' onClick={
+            () => handleDelete(session, tag_id)
+            }>
+              <button>Delete</button>
+          </div>
+        </div> : ""}
     </div>
   )
 }
