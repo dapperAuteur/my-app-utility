ef7c7efbc105834c317a8fa196642667adcb79ad
awe <aweful@awews.com>
2024-09-24 22:04:25 -0400

    use middleware to protect edit/create routes

--- middleware.js
+++ middleware.js
@@ -3,7 +3,14 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
+    // is this middleware working
+    console.log('6 middleware req.nextauth.token.role :>> ', req.nextauth.token.role);
     if ((req.nextUrl.pathname.startsWith("/create") ) && req.nextauth.token.role !== "admin") {      
+      return NextResponse.rewrite(new URL("/denied", req.url));
+    }
+    console.log('10 middleware req.nextauth.token.role :>> ', req.nextauth.token.role);
+    if ((req.nextUrl.pathname.endsWith(`/edit`)) && req.nextauth.token.role !== "admin") {
+      console.log('12 middleware req.nextauth.token.role :>> ', req.nextauth.token.role);
       return NextResponse.rewrite(new URL("/denied", req.url));
     }
   },
@@ -16,10 +23,11 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/create-account",
-    "/create-user",
-    "/create-transaction",
     "/client-member",
+    "/create-account",
+    "/create-tag",
+    "/create-transaction",
+    "/create-user",
     "/member"
   ]
 };
