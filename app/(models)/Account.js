f13b1e7ff2148ef73d4ac475398705038eb79738
awe <aweful@awews.com>
2024-11-25 20:46:55 -0500

    refactor to include tags in Account model

--- app/(models)/Account.js
+++ app/(models)/Account.js
@@ -1,6 +1,7 @@
 "use server"
 
 import mongoose, { Schema } from "mongoose";
+
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
@@ -14,7 +15,12 @@ const accountSchema = new Schema({
     type: String,
     required: true,
     default: "Checking"
-  }
+  },
+  // BUG
+  tags: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Tag"
+  }]
 });
 
 const Account = mongoose.models?.Account || mongoose.model("Account", accountSchema);
